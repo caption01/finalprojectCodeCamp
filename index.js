52df5812b9f9f867bd805336b08ea420bea34b35
nutchapon <nutchapon.han@gmail.com>
2019-10-11 16:40:54 +0700

    third commit for dynamic webpage

---
+++ index.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    $("#updateBtn").click(function(){
+        $(".content p").removeClass("bounce-top");
+        
+        let contentEdit = $("#contentEdit").children("option:selected").val();
+        let infoEdit = $("#infoEdit").val();
+
+        $(`p[id*=${contentEdit}]`).text(infoEdit);
+        $(`p[id*=${contentEdit}]`).addClass("bounce-top")
+        alert(`You have been change the ${contentEdit}`)
+        $("#infoEdit").val("");
+
+    })
+
+    $("#contentSelect").change(function(){
+        $(".content-extra-section div").addClass("active");
+        $(".content-extra-section div").removeClass("bounce-top");
+
+        let selectedContent = $(this).children("option:selected").val();
+        alert(`You wish to look at ..${selectedContent}`)
+
+        $(`div[id*=${selectedContent}]`).removeClass("active");
+        $(`div[id*=${selectedContent}]`).addClass("bounce-top")
+
+    });
+
+});
--- readme.md
+++ readme.md
@@ -8,13 +8,24 @@
 
 ## commit 2 - responsive 
 
-    [x] add responsive for desktop screen
+    [x] add responsive for desktop screen.
         [x] text size.
         [x] text wrap.
         [x] resopnsive image.
-        [x] resopnsive block
+        [x] resopnsive block.
 
-    [x] add media query for small device (575 px) 
+    [x] add media query for small device (575 px). 
         
 ## commit 3 - dynamic
 
+    [x] craete dynamic editor for name, email and education.
+
+    [x] create dynamic infomation content for Ability dropdown menu.
+
+    [x] add some animation for user-experiance.
+        [x] text change bounce.
+        [x] alert popup.
+        [x] Say "hello" picture.
+        [x] button touch feeling.
+
+    [x] final style decoration.
